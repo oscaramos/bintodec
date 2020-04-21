0f1c0e9479ece2c96070fb9add6c26ac6c39c77f
Oscar <oscar.ramos@ucsp.edu.pe>
2020-04-21 02:15:06 -0500

    Updates

---
+++ precache-manifest.ae9427cdc46852ed8f8ae727f27d7d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f94fd29626632c82162557faa2f822",
+    "url": "/bintodec/index.html"
+  },
+  {
+    "revision": "36faa400d414fbb2d718",
+    "url": "/bintodec/static/css/main.251e0600.chunk.css"
+  },
+  {
+    "revision": "847a4d2697dfb9a9b4fe",
+    "url": "/bintodec/static/js/2.27a5681a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bintodec/static/js/2.27a5681a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36faa400d414fbb2d718",
+    "url": "/bintodec/static/js/main.7e6146e9.chunk.js"
+  },
+  {
+    "revision": "e19b03a4eed9425c401c",
+    "url": "/bintodec/static/js/runtime-main.930b3b7c.js"
+  }
+]);
