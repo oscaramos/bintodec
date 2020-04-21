66e6c358f163c6e0650223445fc73464d9a681f5
Oscar <oscar.ramos@ucsp.edu.pe>
2020-04-21 01:35:56 -0500

    Input added and state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="bin">
-      <div className="bin-form">
-        <div className="input-text">10111101</div>
-        <div className='input-line'/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      binary: '10111101',
+      decimal: '377'
+    };
+  }
+
+  render() {
+    const { binary, decimal } = this.state;
+    return (
+      <div className='App'>
+        <div className='bin'>
+          <div className='bin-form'>
+            <input type='text' className='binary-text' value={binary} />
+            <div className='binary-line' />
+          </div>
+        </div>
+        <div className='dec'>
+          <div className='dec-form'>
+            <input type='text' className='decimal-text' value={decimal} />
+            <div className='decimal-line' />
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="dec">
-      <div className="dec-form">
-        <div className="output-text">377</div>
-        <div className='output-line'/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
